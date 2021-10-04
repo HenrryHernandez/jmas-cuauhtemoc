e26224cd86336a74260c476bdcdbeefcfd1cdc74
Jenrri <jenrruhndz@gmail.com>
2021-10-04 06:50:43 -0600

    style (Tabs): add icons for tabs

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import {MainScreen} from '../screens/MainScreen';
 import {colors} from '../theme/appTheme';
@@ -8,8 +9,22 @@ const BottomTabAndroid = createMaterialBottomTabNavigator();
 
 export const Tabs = () => {
   return (
-    <BottomTabAndroid.Navigator barStyle={{backgroundColor: colors.primary}}>
-      <BottomTabAndroid.Screen name="MainScreen" component={MainScreen} />
+    <BottomTabAndroid.Navigator
+      barStyle={{backgroundColor: colors.primary}}
+      screenOptions={({route}) => ({
+        tabBarIcon: () => {
+          let iconName: string = '';
+
+          switch (route.name) {
+            case 'Inicio':
+              iconName = 'home';
+              break;
+          }
+
+          return <Icon name={iconName} size={20} color="white" />;
+        },
+      })}>
+      <BottomTabAndroid.Screen name="Inicio" component={MainScreen} />
     </BottomTabAndroid.Navigator>
   );
 };
