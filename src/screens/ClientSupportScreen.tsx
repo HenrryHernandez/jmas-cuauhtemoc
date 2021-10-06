92394e74992874bdc0f68e63ddbf4dfcf2bb7803
Jenrri <jenrruhndz@gmail.com>
2021-10-05 21:55:21 -0600

    feat (ClientSupportScreen): add call functionality

--- src/screens/ClientSupportScreen.tsx
+++ src/screens/ClientSupportScreen.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Linking, View} from 'react-native';
 
 import {ClientSupportOption, Header} from '../components';
 
 const ClientSupportScreen = () => {
-  const call = () => {
-    console.log('calling');
-  };
-
-  const call2 = () => {
-    console.log('calling 2');
+  const call = (phoneNumber: string) => {
+    Linking.openURL(`tel:${phoneNumber}`);
   };
 
   return (
@@ -20,13 +16,17 @@ const ClientSupportScreen = () => {
         phoneNumber="625 581 4898"
         text1="Atención"
         text2="Telefónica"
-        callPhoneNumber={call}
+        callPhoneNumber={() => {
+          call('614 122 2007');
+        }}
       />
       <ClientSupportOption
         phoneNumber="625 581 4910"
         text1="Reportar fuga"
         text2=""
-        callPhoneNumber={call2}
+        callPhoneNumber={() => {
+          call('614 122 2007');
+        }}
       />
     </View>
   );
