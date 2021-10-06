8db031193c88448d6caa830e551b64f34d953b67
Jenrri <jenrruhndz@gmail.com>
2021-10-05 21:42:21 -0600

    refactor (components/index): order alphabetically

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,3 @@
-export {default as Header} from './Header';
 export {default as Background} from './Background';
 export {default as ClientSupportOption} from './ClientSupportOption';
+export {default as Header} from './Header';
