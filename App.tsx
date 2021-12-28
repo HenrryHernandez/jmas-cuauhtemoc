4752e33d526449a08199b31123ce7006f7981270
Henrry Hernandez <jenrruhndz@gmail.com>
2021-12-28 11:40:39 -0700

    style (App): add SafeAreaView for general margin top in iOS

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native';
+
 import {Navigator} from './src/navigator/navigator';
 
 const App = () => {
-  return <Navigator />;
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Navigator />
+    </SafeAreaView>
+  );
 };
 
 export default App;
