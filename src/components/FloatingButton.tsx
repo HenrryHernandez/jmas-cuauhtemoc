5da3e9d17367bd39eff200784626a02d911ef75d
Henrry Hernandez <jenrruhndz@gmail.com>
2022-01-13 19:22:35 -0700

    style (FloatingButton): add mock up for button for payment

---
+++ src/components/FloatingButton.tsx
@@ -0,0 +1,49 @@
+import React, {useRef} from 'react';
+import {Animated, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import {colors} from '../theme/appTheme';
+
+const FloatingButton = () => {
+  const scale = useRef(new Animated.Value(1)).current;
+
+  const onPressIn = () => {
+    Animated.timing(scale, {
+      toValue: 1.1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const onPressOut = () => {
+    Animated.timing(scale, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}>
+      <Animated.View style={{...styles.button, transform: [{scale}]}}>
+        <Icon color="white" size={45} name="card-sharp" />
+      </Animated.View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+  },
+});
+
+export default FloatingButton;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export {default as Background} from './Background';
 export {default as ClientSupportOption} from './ClientSupportOption';
+export {default as FloatingButton} from './FloatingButton';
 export {default as Header} from './Header';
