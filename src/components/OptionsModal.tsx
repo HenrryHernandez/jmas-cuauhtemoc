3d5a22e7758c5bb12aec67f809a37b9212aa9683
Henrry Hernandez <jenrruhndz@gmail.com>
2022-01-14 17:32:11 -0700

    feat (OptionsModal): add options when main button is pressed

---
+++ src/components/OptionsModal.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface Props {
+  modalVisible: boolean;
+  closeModal: () => void;
+}
+
+const OptionsModal = ({modalVisible, closeModal}: Props) => {
+  return (
+    <View>
+      <Modal transparent={true} visible={modalVisible}>
+        <TouchableWithoutFeedback onPress={closeModal}>
+          <View style={styles.container}>
+            <View style={styles.optionsContainer}>
+              <View style={styles.textContainer}>
+                <Text
+                  style={{
+                    fontSize: 18,
+                  }}>
+                  Selecciona una manera de ingresar tu número de cuenta
+                </Text>
+              </View>
+              <TouchableOpacity
+                style={{
+                  height: 70,
+                }}>
+                <View style={styles.optionContainer}>
+                  <Icon name="create-sharp" size={20} color="black" />
+                  <Text style={styles.optionText}>
+                    Escribir número de cuenta.
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  height: 70,
+                }}>
+                <View style={styles.optionContainer}>
+                  <Icon name="barcode-sharp" size={20} color="black" />
+                  <Text style={styles.optionText}>
+                    Escanear código de barras.
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+  optionsContainer: {
+    height: 200,
+    width: '100%',
+    backgroundColor: 'rgb(255,255,255)',
+    position: 'absolute',
+    bottom: 0,
+  },
+  textContainer: {
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  optionContainer: {
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  optionText: {marginLeft: 20, fontSize: 16},
+});
+
+export default OptionsModal;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export {default as Background} from './Background';
 export {default as ClientSupportOption} from './ClientSupportOption';
 export {default as FloatingButton} from './FloatingButton';
 export {default as Header} from './Header';
+export {default as OptionsModal} from './OptionsModal';
