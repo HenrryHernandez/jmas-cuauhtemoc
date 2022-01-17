5f2539eea59b5b5b5c7b32614a69ae84cf95c963
Henrry Hernandez <jenrruhndz@gmail.com>
2022-01-16 17:20:07 -0700

    feat (UserInformationScreen): add file

---
+++ src/screens/UserInformationScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const UserInformationScreen = () => {
+  return (
+    <View>
+      <Text>UserInformationScreen</Text>
+    </View>
+  );
+};
+
+export default UserInformationScreen;
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,3 +1,4 @@
 export {default as ClientSupportScreen} from './ClientSupportScreen';
 export {default as MainScreen} from './MainScreen';
 export {default as MapScreen} from './MapScreen';
+export {default as UserInformationScreen} from './UserInformationScreen';
